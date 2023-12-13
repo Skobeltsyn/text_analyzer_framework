a61c4f71958534112cfc5314d014922b0fb82f93
konstantin@skobeltsyn.com <konstantin@skobeltsyn.com>
2023-12-13 15:49:05 +0300

    release

--- db/migrate/20231126125214_create_paramater_desired_values.rb
+++ db/migrate/20231126125214_create_paramater_desired_values.rb
@@ -1,6 +1,6 @@
 class CreateParamaterDesiredValues < ActiveRecord::Migration[7.0]
   def change
-    create_table :paramater_desired_values do |t|
+    create_table :parameter_desired_values do |t|
       t.text :value
       t.float :normalized_value
       t.text :commentary
--- db/migrate/20231126204734_add_parameter_name_to_paramater_desired_values.rb
+++ db/migrate/20231126204734_add_parameter_name_to_paramater_desired_values.rb
@@ -1,5 +1,5 @@
 class AddParameterNameToParamaterDesiredValues < ActiveRecord::Migration[7.0]
   def change
-    add_column :paramater_desired_values, :parameter_name, :string
+    add_column :parameter_desired_values, :parameter_name, :string
   end
 end
