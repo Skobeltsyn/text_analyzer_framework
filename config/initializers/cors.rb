810ee418f412c8463f2cb0e5ee2fd408fcbb8e0f
konstantin@skobeltsyn.com <konstantin@skobeltsyn.com>
2023-12-13 15:59:12 +0300

    release

---
+++ config/initializers/cors.rb
@@ -0,0 +1,10 @@
+config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource(
+      '*',
+      headers: :any,
+      methods: [:get, :patch, :put, :delete, :post, :options, :head]
+    )
+  end
+end
