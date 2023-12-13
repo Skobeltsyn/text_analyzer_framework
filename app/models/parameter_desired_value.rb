7310cc6338e061958e74fa887a9d84c25734d512
konstantin@skobeltsyn.com <konstantin@skobeltsyn.com>
2023-12-13 16:04:44 +0300

    release

--- app/models/parameter_desired_value.rb
+++ app/models/parameter_desired_value.rb
@@ -1,3 +1,8 @@
 class ParameterDesiredValue < ApplicationRecord
   belongs_to :conversation_replica
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["commentary", "conversation_replica_id", "created_at", "id", "kase_id", "normalized_value", "parameter_name", "updated_at", "value"]
+  end
+
 end
